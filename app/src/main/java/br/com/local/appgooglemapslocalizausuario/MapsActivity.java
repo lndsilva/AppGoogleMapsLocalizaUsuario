91c995c94460331510b2e636f481bb2acfb1de3f
Laercio-pc <lndsilva@hotmail.com>
2020-07-27 16:36:20 -0300

    Posicionando o marcador com base no endereço do usuário

--- app/src/main/java/br/com/local/appgooglemapslocalizausuario/MapsActivity.java
+++ app/src/main/java/br/com/local/appgooglemapslocalizausuario/MapsActivity.java
@@ -9,6 +9,8 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,6 +23,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -48,15 +54,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -76,9 +73,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 //limpando os marcadores para não repetir no mapa
                 mMap.clear();
 
-                LatLng localUsuario = new LatLng(latitude, longitude);
-                mMap.addMarker(new MarkerOptions().position(localUsuario).title("Local"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 16));
+                /*
+
+                */
+                //Recupera informações do local do usuário
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    //Recupera o endereço do usuário
+                    // List<Address> listaEndereco = geocoder.getFromLocation(latitude, longitude, 1);
+
+                    //Recuperar o local pelo endereço
+                    String enderecoLocal = "AV ROBERTO KENNEDY, 4695 - Interlagos, São Paulo - SP, 04772-005";
+                    List<Address> listaEndereco = geocoder.getFromLocationName(enderecoLocal, 1);
+
+                    //testando se realmente temos um endereço
+                    if (listaEndereco != null && listaEndereco.size() > 0) {
+                        //se quiser utilizar uma estrutura de repetição pode pegar a lista de endereço toda
+                        Address endereco = listaEndereco.get(0);
+                        //Log.d("local", "onLocationChanged: " + endereco.getAddressLine(0));
+
+                        //Posicionando o marcador com base no endereço do usuário
+
+                        Double lat = endereco.getLatitude();
+                        Double lon = endereco.getLongitude();
+
+                        LatLng localUsuario = new LatLng(lat, lon);
+                        mMap.addMarker(new MarkerOptions().position(localUsuario).title("Local"));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario, 16));
+
+
+                        Log.d("local", "onLocationChanged: " + endereco.toString());
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
             }
         };
@@ -93,9 +122,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
     }
+
     //Criando a janela para permissões do usuário a sua localização
-
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
